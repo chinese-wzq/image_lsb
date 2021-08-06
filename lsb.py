71d99ccdf1534a651d777cb7d8acb851530a10b2
yi-ge-shuai-qi-de-kai-fa-zhe <40971680+yi-ge-shuai-qi-de-kai-fa-zhe@users.noreply.github.com>
2021-08-06 11:16:50 +0800

    更新2.0最终版本
    这个版本减少了对图像的破坏，整体图像看上去与原图区别，对图片像素点的更改视要编码的字符串长度而定。然而当要编码的字符串长度过长时，该版本几乎与上一版本表现得差不多，都会出现明显的色彩断层。

--- lsb.py
+++ lsb.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 from PIL import Image
 import sys,time
-print('图片藏文字与读取Demo\nby\t吴宗权\n\n仅支持纯英文，不支持字符的编码！\n\n\n帮助：\n在控制台中使用"python lsb.py 原图片目录 编码后图片目录 编码内容"进行编码\n使用"python lsb.py 编码后文件目录"进行解码')
+print('图片藏文字与读取Demo    2.0\nby\tWZQ|帅in逼|13905069\n\n仅支持纯英文，不支持字符的编码！\n\n\n帮助：\n在控制台中使用"python lsb.py 原图片目录 编码后图片目录 编码内容"进行编码\n使用"python lsb.py 编码后文件目录"进行解码')
 def image_code(sb:str,sbb:str,gwait:str):
     tmp = []
     for c in gwait:
@@ -11,20 +11,26 @@ def image_code(sb:str,sbb:str,gwait:str):
     file = Image.open(sb)
     loaded = file.load()
     count = 0
+    exit=False
     for i in range(file.size[0]):
         for ii in range(file.size[1]):
             fuck = list(loaded[i, ii])
             for iii in range(0, 3):
+                g=list(str(fuck[iii]))
                 if count > len(wait) - 1:
-                    g = list(str(fuck[iii]))
-                    g[len(g) - 1] = "0"
-                    fuck[iii] = int("".join(g))
+                    if g[len(g)-1]!="0" and g[len(g)-1]!="1":
+                        print("生成完毕")
+                        exit=True
+                        break
+                    else:
+                        g[len(g) - 1] = "2"
                 else:
-                    g = list(str(fuck[iii]))
                     g[len(g) - 1] = wait[count]
-                    fuck[iii] = int("".join(g))
+                fuck[iii] = int("".join(g))
                 count = count + 1
             loaded[i, ii]=tuple(fuck)
+            if exit:break
+        if exit:break
     print("图像生成成功，正在保存......")
     file.save(sbb)
     print("保存成功！正在关闭文件......")
@@ -35,13 +41,20 @@ def image_decode(sb:str):
     print("成功打开图片，正在读取......")
     loaded=file.load()
     tmp=[""]
+    exit=False
     for i in range(file.size[0]):
         for ii in range(file.size[1]):
             for iii in range(0, 3):
                 g=list(str(loaded[i,ii][iii]))
+                if g[len(g)-1]!="0" and g[len(g)-1]!="1":
+                    print("读取完毕")
+                    exit=True
+                    break
                 if not len(tmp[len(tmp)-1])<7:
                     tmp.append("")
                 tmp[len(tmp) - 1]=tmp[len(tmp)-1]+g[len(g)-1]
+            if exit:break
+        if exit:break
     print("读取成功，正在分析......")
     for i in range(len(tmp)-1,-1,-1):
         if int(tmp[i])==0:
@@ -55,7 +68,7 @@ def image_decode(sb:str):
     file.close()
     return strla
 if len(sys.argv)==2:
-    print(image_decode(sys.argv[1]))
+    print("保存在图片里面的话是："+image_decode(sys.argv[1]))
 elif len(sys.argv)==4:
     image_code(sys.argv[1], sys.argv[2],sys.argv[3])
 else:
