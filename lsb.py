79617366312ff613dcb6d40493cc9ef9a01d50c9
yi-ge-shuai-qi-de-kai-fa-zhe <40971680+yi-ge-shuai-qi-de-kai-fa-zhe@users.noreply.github.com>
2021-08-09 11:09:21 +0800

    发布3.0ProPlus++ 真·最终版
    通过更好的算法优化，让编码后的图与编码前的图几乎看不出区别！

--- lsb.py
+++ lsb.py
@@ -1,16 +1,20 @@
 # -*- coding: UTF-8 -*-
 from PIL import Image
 import sys,time
-print('图片藏文字与读取Demo    2.0\nby\tWZQ|帅in逼|13905069\n\n仅支持纯英文，不支持字符的编码！\n\n\n帮助：\n在控制台中使用"python lsb.py 原图片目录 编码后图片目录 编码内容"进行编码\n使用"python lsb.py 编码后文件目录"进行解码')
+print('图片藏文字与读取Demo    3.0ProPlus++\nby\tWZQ|帅in逼|13905069\n\n仅支持纯英文，不支持字符的编码！\n\n\n帮助：\n在控制台中使用"python lsb.py 原图片目录 编码后图片目录 编码内容"进行编码\n使用"python lsb.py 编码后文件目录"进行解码\n\n')
+#0 1 2 3 4 5 6 7 8 9
+#用用留留 用 用留留用用
+low_use=["0","1","4","5","8","9"]
+low_unuse=["2","3","6","7"]
 def image_code(sb:str,sbb:str,gwait:str):
-    tmp = []
+    tmp=[]
     for c in gwait:
         tmp.append(bin(ord(c)).replace('0b', ''))
-    wait = list(str(''.join(tmp)))
+    wait=list(str(''.join(tmp)))
     print("编码成功，正在生成图像......")
-    file = Image.open(sb)
-    loaded = file.load()
-    count = 0
+    file=Image.open(sb)
+    loaded=file.load()
+    count=0
     exit=False
     for i in range(file.size[0]):
         for ii in range(file.size[1]):
@@ -18,17 +22,36 @@ def image_code(sb:str,sbb:str,gwait:str):
             for iii in range(0, 3):
                 g=list(str(fuck[iii]))
                 if count > len(wait) - 1:
-                    if g[len(g)-1]!="0" and g[len(g)-1]!="1":
-                        print("生成完毕")
-                        exit=True
-                        break
-                    else:
-                        g[len(g) - 1] = "2"
+                    #多出来像素的处理
+                    #查找最近的数字
+                    a=10000
+                    keep=""
+                    for iiii in low_unuse:
+                        if abs(int(iiii)-int(g[len(g)-1]))<a:
+                            if int(fuck[iii])>=250 and int(iiii)<6:
+                                keep=iiii
+                                a=abs(int(iiii)-int(g[len(g)-1]))
+                            elif int(fuck[iii])<250:
+                                keep=iiii
+                                a=abs(int(iiii)-int(g[len(g)-1]))
+                    g[len(g)-1]=keep
+                    exit=True
                 else:
-                    g[len(g) - 1] = wait[count]
-                fuck[iii] = int("".join(g))
-                count = count + 1
-            loaded[i, ii]=tuple(fuck)
+                    #需使用像素的处理
+                    a=10000
+                    keep=""
+                    for iiii in low_use:
+                        if abs(int(iiii)-int(g[len(g)-1]))<a and int(iiii)%2==int(wait[count])%2:
+                            if int(fuck[iii])>=250 and int(iiii)<6:
+                                keep=iiii
+                                a=abs(int(iiii)-int(g[len(g)-1]))
+                            elif int(fuck[iii])<250:
+                                keep=iiii
+                                a=abs(int(iiii)-int(g[len(g)-1]))
+                    g[len(g)-1]=keep
+                fuck[iii]=int("".join(g))
+                count=count + 1
+            loaded[i,ii]=tuple(fuck)
             if exit:break
         if exit:break
     print("图像生成成功，正在保存......")
@@ -46,13 +69,14 @@ def image_decode(sb:str):
         for ii in range(file.size[1]):
             for iii in range(0, 3):
                 g=list(str(loaded[i,ii][iii]))
-                if g[len(g)-1]!="0" and g[len(g)-1]!="1":
+                if g[len(g)-1] in low_unuse:
                     print("读取完毕")
                     exit=True
                     break
-                if not len(tmp[len(tmp)-1])<7:
-                    tmp.append("")
-                tmp[len(tmp) - 1]=tmp[len(tmp)-1]+g[len(g)-1]
+                else:
+                    if not len(tmp[len(tmp)-1])<7:
+                        tmp.append("")
+                    tmp[len(tmp)-1]=tmp[len(tmp)-1]+str(int(g[len(g)-1])%2)
             if exit:break
         if exit:break
     print("读取成功，正在分析......")
@@ -70,6 +94,6 @@ def image_decode(sb:str):
 if len(sys.argv)==2:
     print("保存在图片里面的话是："+image_decode(sys.argv[1]))
 elif len(sys.argv)==4:
-    image_code(sys.argv[1], sys.argv[2],sys.argv[3])
+    image_code(sys.argv[1],sys.argv[2],sys.argv[3])
 else:
     time.sleep(5)
